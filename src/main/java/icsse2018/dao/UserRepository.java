b02d4feb170464b75be6f926cb84256e320f7719
Lưu Phước Phương Nam <luunamten@gmail.com>
2018-03-29 19:20:33 +0700

    commit

---
+++ src/main/java/icsse2018/dao/UserRepository.java
@@ -0,0 +1,8 @@
+package icsse2018.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import icsse2018.model.User;
+
+public interface UserRepository extends CrudRepository<User,Integer>{
+	User findByUsernameAndPassword(String username, String password);
+}
